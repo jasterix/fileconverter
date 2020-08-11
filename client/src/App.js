b635b66a0667362d11c86c6297be559435103e17
Jasterix <ljcalixte@gmail.com>
2020-08-10 21:05:08 -0400

    basic file structures for client and server

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import React, { Component } from 'react';
+import axios from 'axios';
+import { saveAs } from 'file-saver';
 import './App.css';
 
 function App() {
